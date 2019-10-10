aa18d01679a5967745b889ebbfec1db36c0a0b4d
CristianNina <cristiannina12@gmail.com>
2019-10-10 17:54:02 -0430

    Actualizacion gestinarHerramienta gestionarCotizacion

--- controller/herramientaController.php
+++ controller/herramientaController.php
@@ -1,18 +1,43 @@
 <?php
-require "../model/HerramientaModel.php";
 
-$codigo=$_GET['codigo'];
-$nombre=$_GET['nombre'];
-$Descripcion=$_GET['descripcion'];
-$Estado=$_GET['estado'];
+if (isset($_GET['nombre']) && isset($_GET['descripcion']) && isset($_GET['estado'])) {
+    $nombre=$_GET['nombre'];
+    $Descripcion=$_GET['descripcion'];
+    $Estado=$_GET['estado'];
+    require "../model/HerramientaModel.php";
+    $user = new Herramienta(0,$nombre,$Descripcion,$Estado);
+    $user->codigo = $user->getCantidadHerramienta()+1;
+    if (!$user->insertarHerramienta()) {
+        echo "Error No se pudo registrar la nueva herramienta
+                vuelva a interntarlo";
+    }
+    header('Location: ../view/gestionDeHerramienta/gestionHerramienta.php');
+}
+
+function getListaDeHerramientas(){
+    $usuario1= new Herramienta(0,"","","");
+    $result=$usuario1->getListaHerramientas();
+    $nroFilas=pg_num_rows($result);
+    $printer="";
+    for ($tupla=0; $tupla <$nroFilas ; $tupla++) { 
+        $printer.='<tr> <td>'.pg_result($result,$tupla,0).'</td>';
+        $printer.=      '<td>'.pg_result($result,$tupla,1).'</td>';
+        $printer.=      '<td>'.pg_result($result,$tupla,2).'</td>';
+        $printer.=      '<td>'.pg_result($result,$tupla,3).'</td>';
+        $printer.=      '<td> <div class="btn-group">
+                                            <button type="button" class="btn btn-warning btn-sm" title="Actualizar">
+                                                <i class="fa fa-fw fa-refresh"></i>
+                                            </button>
+                                            &nbsp
+                                            <button type="button" class="btn bg-purple btn-sm" title="Editar">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                      </div>
+                                 </td>
+                          </tr>';
+    }
+    return $printer;
+}
 
 
-//$almacen=new Almacen($codigo,$nombre,$Direccion);
-echo $codigo;
-echo $nombre;
-echo $Descripcion;
-echo $Estado;
-$herramienta=new Herramienta($codigo,$nombre,$Descripcion,$Estado);
-$result=$herramienta->insertarHerramienta();
-header('Location: ../view/gestionDeHerramienta/gestionHerramienta.php');
 ?>
