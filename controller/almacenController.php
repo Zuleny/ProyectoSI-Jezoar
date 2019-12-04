2056e7d322ae0d4e98af9895f945878fc444b83b
zuleny <zuleny.cr@gmail.com>
2019-12-04 18:38:40 -0400

    Insercion en tabla bitacora( gestion personal, gestion proveedor)

--- controller/almacenController.php
+++ controller/almacenController.php
@@ -11,8 +11,17 @@
         if (!$almacen->insertarAlmacen()) {
             echo "Error No se pudo registrar al nuevo almacen
                     vuelva a interntarlo";
+            $errorMessage = "<b>Error en proceso de Registro de almacen</b>";
+            header('Location: ../view/Exceptions/exceptions.php?errorMessage='.$errorMessage);
+        }else{
+            session_start();
+            $fecha_hora = date('j-n-Y G:i:s', time());
+            $username = $_SESSION['user'];
+            $almacen->Conexion->execute("INSERT INTO bitacora(nombre_usuario, descripcion, fecha_hora) 
+                                    VALUES ('$username', 'Registro de Almacen: $nombre', '$fecha_hora');");
+            header('Location: ../view/gestionDeAlmacen/gestionAlmacen.php');
         }
-        header('Location: ../view/gestionDeAlmacen/gestionAlmacen.php');
+
     }    
     function getListaDeAlmacen(){
         require_once "../../model/AlmacenModel.php"; 
